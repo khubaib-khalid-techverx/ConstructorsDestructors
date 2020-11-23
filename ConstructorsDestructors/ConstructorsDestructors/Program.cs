88b31931e373760f3e2bdda7248abd9db2967f4e
khubaib-khalid-techverx <74600712+khubaib-khalid-techverx@users.noreply.github.com>
2020-11-23 13:50:18 +0500

    Add files via upload

---
+++ ConstructorsDestructors/ConstructorsDestructors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConstructorsDestructors
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            employee e = new employee();
+            employee e1 = new employee();
+
+        }
+    }
+}
---
+++ ConstructorsDestructors/ConstructorsDestructors/employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorsDestructors
+{
+    class employee
+    {
+       
+        public employee()
+        {
+            Console.WriteLine("Constructor invoked");
+
+        }
+        ~employee()
+        {
+            Console.WriteLine("Destructor Invoked");
+        }
+    }
+}
